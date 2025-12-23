716778f60025d09cf641acc852b5f7528e9f56d5
Umer Saeed <umersaeed4@gmail.com>
2025-12-23 18:32:35 +0100

    updated the api url

--- clientui/src/config.js
+++ clientui/src/config.js
@@ -2,8 +2,8 @@
 // Change the API_BASE value here to switch between local and hosted environments
 
 // For hosted/production:
-//export const API_BASE = "https://wavephysics.onrender.com";
+export const API_BASE = "https://wavephysics.onrender.com";
 
 // For local development:
-export const API_BASE = "http://127.0.0.1:8000";
+//export const API_BASE = "http://127.0.0.1:8000";
 
